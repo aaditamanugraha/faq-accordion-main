c22c5952a2ff4faae479a06fa594c6e48f0c35f3
aaditamanugraha <adhityaaditaman@gmail.com>
2023-12-24 23:56:47 +0800

    Get class faq-card into JS

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 let test = "Hello World!";
+
+const faqCard = document.getElementsByClassName("faq-card");
+
+for (let i = 0; i < faqCard.length; i++) {
+  faqCard[i].addEventListener("click", () => {
+    console.log("Berhasil");
+  });
+}
