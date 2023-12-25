3ee04167df9a184ef8d6d229975efa3b86e98772
aaditamanugraha <adhityaaditaman@gmail.com>
2023-12-25 14:32:10 +0800

    Change icon when accordion open

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (answer.style.display === "block") {
         answer.style.display = "none";
+        icon.src = "./assets/images/icon-plus.svg";
       } else {
         answer.style.display = "block";
+        icon.src = "./assets/images/icon-minus.svg";
       }
     });
   });
